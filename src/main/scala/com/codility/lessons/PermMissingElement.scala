a7a6fbd0b67c4f5c1d0f3d9349e8c38aede6973f
Florin Bunau <florin.bunau@gmail.com>
2019-09-16 13:20:22 +0300

    Added solution

--- src/main/scala/com/codility/lessons/PermMissingElement.scala
+++ src/main/scala/com/codility/lessons/PermMissingElement.scala
@@ -3,7 +3,10 @@ package com.codility.lessons
 object PermMissingElement {
 
   def solution(a: Array[Int]): Int = {
-    0
+    if (a.length == 0) return 1
+    val x = (1 to (a.length + 1)).reduce(_ ^ _)
+    val y = a.reduce(_ ^ _)
+    x ^ y
   }
 
 }
